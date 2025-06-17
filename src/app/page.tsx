2acf67c20ce857c7731e351eafab80ec488ef390
Ashutosh <ashuma2721@gmail.com>
2025-06-17 12:49:22 +0530

    minor updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,23 @@ import Link from "next/link";
 export default function Home() {
   return (
       <main className={styles.main}>
-         <Link href="/search">Search Country Details by country name</Link>
-          <Link href="/search">Search Country Details by country phone code</Link>
+          <fieldset>
+              <legend>
+                  Query country by
+              </legend>
+              <div>
+                  <Link href="/search">Search Country Details by country name</Link>
+              </div>
+              <div>
+
+              <Link href="/search">Search Country Details by country phone code</Link>
+          </div>
+              <div>
+
           <Link href="/search">Search Country Details by country code</Link>
+      </div>
+
+</fieldset>
       </main>
   );
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,17 @@
 export default function Search() {
     return (
         <div>
-            <main>Search</main>
+            <main>
+                <form>
+                    <fieldset>
+                        <legend>Search country by name</legend>
+                        <div>
+                            <label htmlFor="name-based">Enter name</label>
+                            <input name="query-name" id="name-based" type="text" placeholder="Enter name" required  minLength={3} />
+                        </div>
+                    </fieldset>
+                </form>
+            </main>
         </div>
     )
 }
